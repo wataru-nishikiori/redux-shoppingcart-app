43ed866c8be85a64c2c281e2dc7a6600cdf23f75
TN910214 <takahiro.nakano@ysinc.co.jp>
2025-04-08 16:29:44 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import { calculateTotals } from './features/cart/CartSlice';
 function App() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems, dispatch]);
 
   return (
     <main>
-    <Modal />
-     <Navbar /> 
+      {isOpen && <Modal />}
+      <Navbar /> 
       <CartContainer />
     </main>
   );
--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import CartItem from './CartItem';
-import { clearCart } from '../features/cart/CartSlice';
+import { openModal } from '../features/modal/ModalSlice';
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -34,7 +34,7 @@ const CartContainer = () => {
             合計<span>{total}円</span>
           </h4>
         </div>
-        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>全削除</button>
+        <button className="btn clear-btn" onClick={() => dispatch(openModal())}>全削除</button>
       </footer>
     </section>
   )
