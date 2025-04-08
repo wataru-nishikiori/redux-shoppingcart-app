dcf17d3d599e007b3e51b3cb7b3e6d12b3212587
TN910214 <takahiro.nakano@ysinc.co.jp>
2025-04-08 16:18:52 +0900

    途中経過

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import Modal from './components/Modal';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <Modal />
+     <Navbar /> 
+      <CartContainer />
+    </main>
   );
 }
 
---
+++ src/components/CartContainer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import CartItem from './CartItem';
+import { clearCart } from '../features/cart/CartSlice';
+
+const CartContainer = () => {
+  const dispatch = useDispatch();
+  const { amount, cartItems, total } = useSelector((state) => state.cart);
+  if(amount < 1) {
+    return(
+      <section className="cart">
+        <header>
+          <h2>買い物カゴ</h2>
+          <h4 className="empty-cart">何も入っていません・・・;_;</h4>
+        </header>
+      </section>
+    )
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>買い物カゴ</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item}/>;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            合計<span>{total}円</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>全削除</button>
+      </footer>
+    </section>
+  )
+}
+
+export default CartContainer
